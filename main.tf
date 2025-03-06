483020d311467f34d9364c8a6ebf21eb6bbf8ceb
vinay-uppara <vinaykumaruppara36@gmail.com>
2025-03-06 17:26:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws"{
 region = "us-east-1"
 }
 
-resource "aws_instance" "name"{
+resource "aws_instance" "vinu"{
 ami = "ami-05b10e08d247fb927"
 instance_type = "t2.micro"
 key_name = "vin"
